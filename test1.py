a50d2af80df9f6d3fcf0a98dcbe9d70d455104ff
sho0529-codes <sho0529.codes@gmail.com>
2025-09-05 23:15:43 +0900

    test1.py

--- test1.py
+++ test1.py
@@ -19,17 +19,30 @@ def read_file(file_path: str) -> str:
     """
     with open(file_path, "r", encoding="utf-8") as f:
         text = f.read()
+    print(f"{file_path} is read")
 
     return text
 
-# print(text)
-# 普通に指定するだけのとき
-text = read_file("README.md")  # 読み込むファイルのパスを指定
-lst = ["### ", "## ", "# ", "- ", " "]  # 「#」は多い順で指定して、空白まで含めた方が良いっぽい
-print(extract_char(text, lst))
+def save_file(file_path: str, text: str) -> str:
+    """
+    引き数：file_path（保存するファイルのパス）, text（保存するテキスト）
+    返り値：text（保存したテキスト）
+    """
+    with open(file_path, "w", encoding="utf-8") as f:
+        f.write(text)
+    print(f"{file_path} is saved")
 
-# 正規表現を使って[]や()を取り除くとき
-# text = read_file("README.md")  # 省略
-# lst = ["### ", "## ", "# ", "- ", " ", r"\[.*?\]", r"\(.*?\)",]  # 「[]」や「()」を単なる文字として認識するための「\」、出来るだけ小さい範囲で探すために「?」を付けた方が良いらしい
-lst = ["### ", "## ", "# ", "- ", " ", r"\[", r"\]", r"\(.*?\)",]  # 消しすぎたとこ（[]()の並びだと文字が残らなくて項目が分からなくなる）を修正したやつ
-print(extract_char(text, lst))
+    return text
+
+text = read_file("README.md")  # 読み込むファイルのパスを指定
+lst1 = ["### ", "## ", "# ", "- ", " "]  # 「#」は多い順で指定して、空白まで含めた方が良いっぽい
+# lst2 = ["### ", "## ", "# ", "- ", " ", r"\[.*?\]", r"\(.*?\)",]  # 「[]」や「()」を単なる文字として認識するための「\」、出来るだけ小さい範囲で探すために「?」を付けた方が良いらしい
+lst2 = ["### ", "## ", "# ", "- ", " ", r"\[", r"\]", r"\(.*?\)",]  # 消しすぎたとこ（[]()の並びだと文字が残らなくて項目が分からなくなる）を修正したやつ
+
+out1 = extract_char(text, lst1)  # 普通に指定するだけのとき
+save_file("test1_output1.txt", out1)
+# print(out1)
+
+out2 = extract_char(text, lst2)  # 正規表現を使って[]や()を取り除くとき
+save_file("test1_output2.txt", out2)
+# print(out2)
